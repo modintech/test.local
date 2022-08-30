76fb8d355c64f200a2e3b3e76db08654567aee5e
modintech <offce@modintech.com>
2022-08-30 19:34:47 +0300

    Added folder CSS and file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="">
+<meta name="robots" CONTENT="noindex" />
+<!--link rel="stylesheet" href="/css/normalize.css" type="text/css" /-->
+<link rel="stylesheet" href="/css/style.css" type="text/css" />
+<title>WSEngine 2.0 тест</title>
+</head>
+<body>
+
+</body>
+</html>
